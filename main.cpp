c90b3bb898683bd5151327f3f586ce75127838f5
Hatemmamdoh <36468246+Hatemmamdoh@users.noreply.github.com>
2018-11-17 17:23:23 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 using namespace std;
-
 int sumofdigit (int n)
 {
   int sum = 0 ;
@@ -10,13 +9,20 @@ int sumofdigit (int n)
   sum = (n % 10 + sumofdigit(n / 10)) ;
   return sum ;
 }
-
+int DigitalRoot (int n)
+{
+  int r = 0 ;
+  if (n/10 == 0)
+    return n ;
+  r = DigitalRoot (sumofdigit (n)) ;
+  return r ;
+}
 int main()
 {
-
 int number ;
 cout << "please enter the number :" ;
 cin >> number ;
-cout << "The sum is: " << sumofdigit (number) << endl;
-return 0;
+cout << "The Digital Root is: " << DigitalRoot(number) << endl ;
+
+    return 0;
 }
